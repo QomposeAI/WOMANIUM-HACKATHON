6896f4ed8729875e252f9be985a8107bb6f1d3e6
niteshbansal07 <110964678+niteshbansal07@users.noreply.github.com>
2022-08-20 16:59:09 -0400

    changes to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+import requests
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, jsonify
 
 app = Flask(__name__)
 
@@ -11,3 +12,20 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+headers = {
+    'Key': 'FOgbsGOPiC0Pf2DcMsn9oZDG6301428b',
+}
+
+@app.route("/upload_video", methods=["GET", "POST"])
+def upload_video():
+    if request.method == "POST":
+        if request.files:
+            video = request.files["video"]
+            files = {
+                'file': open(video, 'rb')
+            }
+            response = requests.post('https://muse.ai/api/files/upload', headers=headers, files=files)
+
+            return redirect(request.url)
+    return render_template("templates/index.html")
