ddc780ba9581ab79df96bf8607214d3c1bcec7fe
niteshbansal07 <110964678+niteshbansal07@users.noreply.github.com>
2022-08-23 17:25:01 -0400

    updated specNplot

--- specNplot.py
+++ specNplot.py
@@ -10,24 +10,26 @@ import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
 
-file = "C:/Users/Yap/Desktop/specweb/demogecko.wav" #define path audio file
 
-test, sr = librosa.load(file,sr=16000) #librosa audioload
+def callspec():
+    file = "static/uploads/audio/extracted_audio.wav" #define path audio file
 
-FRAME_SIZE = 400 #define params for stft these are optimal for 16000
-HOP_SIZE = 160
+    test, sr = librosa.load(file,sr=16000) #librosa audioload
 
-def plot_spectrogram(spec, sr, hop_length, y_axis="linear"): # the plotting function can change params here
-    plt.figure(figsize=(19, 1.25))
-    librosa.display.specshow(spec, 
-                             sr=sr, 
-                             hop_length=hop_length, 
-                             x_axis="time", 
-                             y_axis=y_axis, cmap='viridis' ) # change color with cmap change plot size with fig size
-    #plt.colorbar(format="%+2.f") this can be uncommented it shows the colorbar info
+    FRAME_SIZE = 400 #define params for stft these are optimal for 16000
+    HOP_SIZE = 160
 
- #here i do the stft to the audio file
-file = librosa.stft(test, n_fft=FRAME_SIZE, hop_length=HOP_SIZE) 
-Y_test = librosa.power_to_db(np.abs(file) ** 2) #increase amplitude in db for bettew view
+    def plot_spectrogram(spec, sr, hop_length, y_axis="linear"): # the plotting function can change params here
+        plt.figure(figsize=(19, 1.25))
+        librosa.display.specshow(spec, 
+                                sr=sr, 
+                                hop_length=hop_length, 
+                                x_axis="time", 
+                                y_axis=y_axis, cmap='viridis' ) # change color with cmap change plot size with fig size
+        #plt.colorbar(format="%+2.f") this can be uncommented it shows the colorbar info
 
-plot_spectrogram(Y_test, sr, HOP_SIZE,y_axis='log') #here's freq view remove y_axis='log for linear view
+    #here i do the stft to the audio file
+    file = librosa.stft(test, n_fft=FRAME_SIZE, hop_length=HOP_SIZE) 
+    Y_test = librosa.power_to_db(np.abs(file) ** 2) #increase amplitude in db for bettew view
+    plot_spectrogram(Y_test, sr, HOP_SIZE,y_axis='log') #here's freq view remove y_axis='log for linear view
+    plt.savefig('sp_xyz.jpg', dpi=300, frameon='true')
