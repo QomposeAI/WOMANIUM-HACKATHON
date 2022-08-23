be69ceb76302970960dde6d3e84bc254b537d283
Yapraks <110999989+Yapraks@users.noreply.github.com>
2022-08-23 07:28:35 +0200

    generate spectogram for webview

---
+++ specNplot.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 07:08:12 2022
+
+@author: Yap
+"""
+
+import librosa #python librosa install needed
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+
+file = "C:/Users/Yap/Desktop/specweb/demogecko.wav" #define path audio file
+
+test, sr = librosa.load(file,sr=16000) #librosa audioload
+
+FRAME_SIZE = 400 #define params for stft these are optimal for 16000
+HOP_SIZE = 160
+
+def plot_spectrogram(spec, sr, hop_length, y_axis="linear"): # the plotting function can change params here
+    plt.figure(figsize=(19, 1.25))
+    librosa.display.specshow(spec, 
+                             sr=sr, 
+                             hop_length=hop_length, 
+                             x_axis="time", 
+                             y_axis=y_axis, cmap='viridis' ) # change color with cmap change plot size with fig size
+    #plt.colorbar(format="%+2.f") this can be uncommented it shows the colorbar info
+
+ #here i do the stft to the audio file
+file = librosa.stft(test, n_fft=FRAME_SIZE, hop_length=HOP_SIZE) 
+Y_test = librosa.power_to_db(np.abs(file) ** 2) #increase amplitude in db for bettew view
+
+plot_spectrogram(Y_test, sr, HOP_SIZE,y_axis='log') #here's freq view remove y_axis='log for linear view
